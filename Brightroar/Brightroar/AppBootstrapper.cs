4dfded0cf0602b3b17906643605059c6c1ea48aa
Chris <chris.reflection@gmail.com>
2017-06-11 17:17:24 +0100

    Basic VM -> View setup.

---
+++ Brightroar/Brightroar/AppBootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using Brightroar.ViewModels;
+using Caliburn.Micro;
+
+namespace Brightroar
+{
+    public class AppBootstrapper : BootstrapperBase
+    {
+        public AppBootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
